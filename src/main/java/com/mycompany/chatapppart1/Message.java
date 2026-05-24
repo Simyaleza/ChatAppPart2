578065ae24afe52b094eff296f47ffb3f616ef98
Simyaleza <ST10512012@rcconnect.edu.za>
2026-05-24 15:43:59 +0200

    Section 4 complete

--- src/main/java/com/mycompany/chatapppart1/Message.java
+++ src/main/java/com/mycompany/chatapppart1/Message.java
@@ -10,4 +10,55 @@ package com.mycompany.chatapppart1;
  */
 public class Message {
     
+    // attributes for the message class
+    private String messageID; // Used for every message to uniquely identify
+    private int messageNumber; // current msg we on 
+    private String recipient; // To know who is reciving the message
+    private String messageText; // The content of the msg
+    private String messageHash; // Hashing the message
+    
+    
+    // constructor
+    public Message(String msgID, int msgNum, String reciver, String Text ){
+        messageID = msgID;
+        messageNumber = msgNum;
+        recipient = reciver;
+        messageText = Text;
+    }
+    
+    // ------- checking if ID is less than 11 characters
+    public boolean CheckMessageID(){
+        
+    }
+    
+    
+    // --------- validating phone number
+    public String  checkRecipientCell(){
+        
+    }
+    
+    // ---------- hashes the message
+    public String createMessageHash(){
+        
+    }
+    
+    // ask the user to send, delete or store the message
+    public String sentMessage(){
+        
+    }
+    
+    // displays every message sent 
+    public String printMessages(){
+        
+    }
+    
+    // returns the count of messages sent 
+    public int returnTotalMessages(){
+        
+    }
+    
+    private void stroreMessage(){
+        
+    }
+    
 }
